7f994c6d24e421d7784c650a11c37433e7b1b766
romaznova <romaznova@gmail.com>
2021-03-22 21:49:11 +0200

    add hero component

---
+++ src/components/Hero/index.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import styled from "styled-components";
+import Img from "gatsby-image";
+// import styles from "./styles.css";
+
+// Import Swiper styles
+// import "swiper/swiper.scss";
+const HeroComponent = styled.div`
+  display: block;
+  width: 100%;
+  position: relative;
+
+  &::after {
+    display: block;
+    content: "";
+    width: 100%;
+    padding-bottom: 56.5%;
+  }
+
+  .swiper-container {
+    margin-left: auto;
+    margin-right: auto;
+    position: relative;
+    overflow: hidden;
+    list-style: none;
+    padding: 0;
+    /* Fix of Webkit flickering */
+    z-index: 1;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+  }
+
+  .swiper-wrapper {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+    display: flex;
+    transition-property: transform;
+    box-sizing: content-box;
+  }
+  .swiper-slide {
+    flex-shrink: 0;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    transition-property: transform;
+  }
+  .slide-box {
+    max-width: 800px;
+    width: 100%;
+    text-align: center;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 2;
+  }
+  .slide-title,
+  .slide-description {
+    color: #fff;
+    font-size: 24px;
+  }
+  .slide-description {
+    font-size: 18px;
+  }
+  .slide-image {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const Hero = ({ slides }) => {
+  return (
+    <HeroComponent>
+      <Swiper
+        spaceBetween={0}
+        slidesPerView={1}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+        loop="true"
+      >
+        {slides.map(({ title, description, image }, i) => (
+          <SwiperSlide className="swiper-slide" key={i}>
+            <div className="slide-box">
+              <h2 className="slide-title">{title}</h2>
+              <h3 className="slide-description">{description}</h3>
+            </div>
+            <Img
+              className="slide-image"
+              fluid={image.childImageSharp.fluid}
+              alt={`Image ${title}`}
+            />
+          </SwiperSlide>
+        ))}
+        {/* <SwiperSlide className="swiper-slide">Slide 1</SwiperSlide>
+        <SwiperSlide className="swiper-slide">Slide 2</SwiperSlide>
+        <SwiperSlide className="swiper-slide">Slide 3</SwiperSlide>
+        <SwiperSlide className="swiper-slide">Slide 4</SwiperSlide> */}
+      </Swiper>
+    </HeroComponent>
+  );
+};
+
+export default Hero;
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,22 +1,18 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
 
-import Layout from '../components/Layout'
-import Features from '../components/Features'
-import BlogRoll from '../components/BlogRoll'
+import Layout from "../components/Layout";
+import Features from "../components/Features";
+import BlogRoll from "../components/BlogRoll";
+import Hero from "../components/Hero";
 
-export const IndexPageTemplate = ({
-  image,
-  title,
-  heading,
-  subheading,
-  mainpitch,
-  description,
-  intro,
-}) => (
-  <div>
-    <div
+export const IndexPageTemplate = ({ slides }) => {
+  // console.log(props);
+  return (
+    <div>
+      <Hero slides={slides} />
+      {/* <div
       className="full-width-image margin-top-0"
       style={{
         backgroundImage: `url(${
@@ -28,23 +24,23 @@ export const IndexPageTemplate = ({
     >
       <div
         style={{
-          display: 'flex',
-          height: '150px',
-          lineHeight: '1',
-          justifyContent: 'space-around',
-          alignItems: 'left',
-          flexDirection: 'column',
+          display: "flex",
+          height: "150px",
+          lineHeight: "1",
+          justifyContent: "space-around",
+          alignItems: "left",
+          flexDirection: "column",
         }}
       >
         <h1
           className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+            backgroundColor: "rgb(255, 68, 0)",
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
           }}
         >
           {title}
@@ -53,11 +49,11 @@ export const IndexPageTemplate = ({
           className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
           style={{
             boxShadow:
-              'rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px',
-            backgroundColor: 'rgb(255, 68, 0)',
-            color: 'white',
-            lineHeight: '1',
-            padding: '0.25em',
+              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+            backgroundColor: "rgb(255, 68, 0)",
+            color: "white",
+            lineHeight: "1",
+            padding: "0.25em",
           }}
         >
           {subheading}
@@ -110,39 +106,49 @@ export const IndexPageTemplate = ({
           </div>
         </div>
       </div>
-    </section>
-  </div>
-)
+    </section> */}
+    </div>
+  );
+};
 
 IndexPageTemplate.propTypes = {
-  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-  title: PropTypes.string,
-  heading: PropTypes.string,
-  subheading: PropTypes.string,
-  mainpitch: PropTypes.object,
-  description: PropTypes.string,
-  intro: PropTypes.shape({
-    blurbs: PropTypes.array,
-  }),
-}
+  // image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  // title: PropTypes.string,
+  // heading: PropTypes.string,
+  // subheading: PropTypes.string,
+  // mainpitch: PropTypes.object,
+  // description: PropTypes.string,
+  // intro: PropTypes.shape({
+  //   blurbs: PropTypes.array,
+  // }),
+  slides: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      description: PropTypes.string,
+      image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    })
+  ),
+};
 
 const IndexPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark
+  const { frontmatter } = data.markdownRemark;
+  console.log({ data });
 
   return (
     <Layout>
       <IndexPageTemplate
-        image={frontmatter.image}
-        title={frontmatter.title}
-        heading={frontmatter.heading}
-        subheading={frontmatter.subheading}
-        mainpitch={frontmatter.mainpitch}
-        description={frontmatter.description}
-        intro={frontmatter.intro}
+        slides={frontmatter.slides}
+        // image={frontmatter.image}
+        // title={frontmatter.title}
+        // heading={frontmatter.heading}
+        // subheading={frontmatter.subheading}
+        // mainpitch={frontmatter.mainpitch}
+        // description={frontmatter.description}
+        // intro={frontmatter.intro}
       />
     </Layout>
-  )
-}
+  );
+};
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -150,44 +156,28 @@ IndexPage.propTypes = {
       frontmatter: PropTypes.object,
     }),
   }),
-}
+};
 
-export default IndexPage
+export default IndexPage;
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
-        title
-        image {
-          childImageSharp {
-            fluid(maxWidth: 2048, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
-        heading
-        subheading
-        mainpitch {
-          title
+        slides {
           description
-        }
-        description
-        intro {
-          blurbs {
-            image {
-              childImageSharp {
-                fluid(maxWidth: 240, quality: 64) {
-                  ...GatsbyImageSharpFluid
-                }
+          image {
+            childImageSharp {
+              fluid {
+                src
+                srcSet
+                tracedSVG
               }
             }
-            text
           }
-          heading
-          description
+          title
         }
       }
     }
   }
-`
+`;
